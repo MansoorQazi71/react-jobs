574d329c65383aa3f58d1e8ea26133bef96b156a
Mansoor Ahmad Qazi <mansoorqazi71@gmail.com>
2024-09-10 17:37:02 +0500

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const App = () => {
   const names = ["mansoor", "shaheer", "hashir"];
+  const loggedIn = false;
   return (
     <>
       <div className="text-5xl">App</div>
       <ul>{names.map((name,index) => (<li key={index}>{name}</li>))}</ul>
+      {loggedIn ? <h1>WElcome</h1> : <h1>Continue as guest</h1>}
     </>
   );
 };
